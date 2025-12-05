509410e8686aef141ced72498db7ffd5734dcc48
Abhishek Singh <abhis@MIKEY.localdomain>
2025-12-05 16:41:26 +0000

    day56 completed

---
+++ day056/590027542-AbhishekSingh-056-Q106.c
@@ -0,0 +1,35 @@
+// Q106 (Logic Enhancers)
+// Print next greater element for each array element (brute force).
+// Output comma separated.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int next = -1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                next = arr[j];
+                break;
+            }
+        }
+        printf("%d", next);
+        if (i < n - 1)
+            printf(",");
+    }
+
+    return 0;
+}
