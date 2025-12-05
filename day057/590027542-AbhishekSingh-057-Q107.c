bd77b845631c8a021903ed43adccc9398764c4b7
Abhishek Singh <abhis@MIKEY.localdomain>
2025-12-05 16:43:28 +0000

    day57 completed

---
+++ day057/590027542-AbhishekSingh-057-Q107.c
@@ -0,0 +1,35 @@
+// Q107 (Logic Enhancers)
+// Print previous greater element for each array element (brute force).
+// Output comma separated.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int prev = -1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > arr[i])
+            {
+                prev = arr[j];
+                break;
+            }
+        }
+        printf("%d", prev);
+        if (i < n - 1)
+            printf(",");
+    }
+
+    return 0;
+}
