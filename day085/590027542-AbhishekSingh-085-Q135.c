2c052722afdcf67322be015e4f572b081d32a84f
Abhishek Singh <abhis@MIKEY.localdomain>
2025-12-06 12:58:08 +0000

    day85 completed

---
+++ day085/590027542-AbhishekSingh-085-Q135.c
@@ -0,0 +1,18 @@
+// Q135 (Enum)
+// Assign explicit values starting from 10 and print them.
+
+#include <stdio.h>
+
+enum Example
+{
+    A = 10,
+    B,
+    C,
+    D
+};
+
+int main()
+{
+    printf("%d %d %d %d", A, B, C, D);
+    return 0;
+}
